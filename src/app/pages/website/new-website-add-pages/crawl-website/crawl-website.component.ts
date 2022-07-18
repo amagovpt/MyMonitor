eaa14cfa939f1c37f80585bae8561dcd47c17859
Estriga97 <fc47839@alunos.fc.ul.pt>
2022-07-18 15:16:24 +0100

    added css to manual insert

--- src/app/pages/website/new-website-add-pages/crawl-website/crawl-website.component.ts
+++ src/app/pages/website/new-website-add-pages/crawl-website/crawl-website.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectorRef,
+} from "@angular/core";
+
+import { MatDialog } from "@angular/material/dialog";
+import { CrawlerResultsDialogComponent } from 'src/app/dialogs/crawler-results-dialog/crawler-results-dialog.component';
+import { MessageService } from 'src/app/services/message.service';
+import { MonitorService } from 'src/app/services/monitor.service';
+
+
+
 
 @Component({
   selector: 'app-crawl-website',
@@ -7,9 +22,94 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrawlWebsiteComponent implements OnInit {
 
-  constructor() { }
+  @Input("website") website: string;
+  @Output("addPages") addWebsitePages = new EventEmitter<any>();
 
-  ngOnInit(): void {
+  crawlStatus: string;
+  crawlButtonDisable: boolean;
+  crawlResultsDisabled: boolean;
+  startingUrl: string;
+
+
+  constructor(
+    private monitor: MonitorService,
+    private dialog: MatDialog,
+    private cd: ChangeDetectorRef
+  ) {
+
+    this.crawlStatus = "not_running";
+    this.crawlButtonDisable = false;
+    this.crawlResultsDisabled = true;
+
   }
 
+  ngOnInit(): void {
+    this.monitor
+      .getWebsiteStartingUrl(this.website)
+      .subscribe((startingUrl) => {
+        if (startingUrl) {
+          this.startingUrl = startingUrl;
+
+          this.monitor.checkCrawler(this.startingUrl).subscribe((result) => {
+            if (result !== null) {
+              if (result) {
+                this.crawlStatus = "complete";
+                this.crawlButtonDisable = true;
+                this.crawlResultsDisabled = false;
+              } else {
+                this.crawlStatus = "progress";
+                this.crawlButtonDisable = true;
+                this.crawlResultsDisabled = true;
+              }
+            }
+          });
+        }
+      });
+  }
+
+  crawlWebsite(): void {
+    this.monitor.crawlWebsite(this.startingUrl).subscribe((result) => {
+      if (result) {
+        this.crawlStatus = "progress";
+        this.crawlButtonDisable = true;
+      } else {
+        alert("Error");
+      }
+
+      this.cd.detectChanges();
+    });
+  }
+
+  openCrawlingResultsDialog(): void {
+    const dialog = this.dialog.open(CrawlerResultsDialogComponent, {
+      width: "60vw",
+      data: {
+        startingUrl: this.startingUrl,
+      },
+    });
+
+    dialog.afterClosed().subscribe((data) => {
+      if (data) {
+        this.addWebsitePages.next({
+          startingUrl: this.startingUrl,
+          urls: data,
+        });
+      }
+    });
+  }
+
+  deleteCrawlingResults(): void {
+    this.monitor.deleteCrawlingResults(this.startingUrl).subscribe((result) => {
+      if (result) {
+        this.crawlStatus = "not_running";
+        this.crawlButtonDisable = false;
+        this.crawlResultsDisabled = true;
+      } else {
+        alert("Error");
+      }
+
+      this.cd.detectChanges();
+    });
+  }
 }
+
--- src/app/pages/website/new-website-add-pages/manually-add-pages/manually-add-pages.component.ts
+++ src/app/pages/website/new-website-add-pages/manually-add-pages/manually-add-pages.component.ts
@@ -1,5 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormGroupDirective,
+  NgForm,
+} from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+import { MonitorService } from "src/app/services/monitor.service";
 
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    control: FormControl | null,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
+  }
+}
+
+class DomainUrlValidation {
+  static UrlMatchDomain(AC: AbstractControl) {
+    const startingUrl = AC.get("startingUrl").value;
+
+    const urls = AC.get("pages")
+      .value.split("\n")
+      .filter((a) => a !== "");
+
+    let invalid = false;
+    const size = urls.length;
+
+    if (!size) {
+      return null;
+    }
+
+    for (let i = 0; i < size; i++) {
+      const url = urls[i].trim();
+
+      if (!url.startsWith(startingUrl)) {
+        invalid = true;
+      }
+    }
+
+    if (invalid) {
+      AC.get("pages").setErrors({ domainNoMatch: true });
+    } else {
+      return null;
+    }
+  }
+}
 @Component({
   selector: 'app-manually-add-pages',
   templateUrl: './manually-add-pages.component.html',
@@ -7,9 +70,99 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManuallyAddPagesComponent implements OnInit {
 
-  constructor() { }
+  @Input("website") website: string;
+  @Output("addPages") addWebsitePages = new EventEmitter<any>();
 
-  ngOnInit(): void {
+  matcher: ErrorStateMatcher;
+
+  pagesForm: FormGroup;
+  startingUrl: string;
+
+
+  constructor(
+    private monitor: MonitorService,
+    private fb: FormBuilder,
+  ) {
+    this.pagesForm = this.fb.group(
+      {
+        startingUrl: new FormControl({ value: "", disabled: true }),
+        pages: new FormControl("", [
+          Validators.required,
+          urlValidator,
+          missingProtocol,
+        ]),
+      },
+      { validator: DomainUrlValidation.UrlMatchDomain }
+    );
+    this.matcher = new MyErrorStateMatcher();
   }
 
+  ngOnInit(): void {
+    this.monitor
+      .getWebsiteStartingUrl(this.website)
+      .subscribe((startingUrl) => {
+        if (startingUrl) {
+          this.startingUrl = startingUrl;
+          this.pagesForm.controls.startingUrl.setValue(startingUrl);
+        }
+      });
+  }
+
+  addPages(e): void {
+    e.preventDefault();
+
+    const pages = this.pagesForm.value.pages
+      .split("\n")
+      .filter((a) => a !== "")
+      .filter((value, index, self) => self.indexOf(value) === index)
+      .map((p) => {
+        return p.trim();
+      });
+
+    this.addWebsitePages.next({ startingUrl: this.startingUrl, urls: pages });
+  }}
+
+function missingProtocol(control: FormControl) {
+  const urls = control.value.split("\n").filter((a) => a !== "");
+
+  let invalid = false;
+  const size = urls.length;
+
+  if (!size) {
+    return null;
+  }
+
+  for (let i = 0; i < size; i++) {
+    const url = urls[i].trim();
+
+    if (!url.startsWith("http://") && !url.startsWith("https://")) {
+      invalid = true;
+      break;
+    }
+  }
+
+  return invalid ? { missingProtocol: { value: true } } : null;
 }
+
+function urlValidator(control: FormControl) {
+  const urls = control.value.split("\n").filter((a) => a !== "");
+
+  let invalid = false;
+  const size = urls.length;
+
+  if (!size) {
+    return null;
+  }
+
+  for (let i = 0; i < size; i++) {
+    const url = urls[i].trim();
+
+    if (!url.includes(url, ".") || url[url.length - 1] === ".") {
+      invalid = true;
+      break;
+    }
+  }
+
+  return invalid ? { url: { value: true } } : null;
+}
+
